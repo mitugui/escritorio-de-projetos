fb43fdc6ea4ce8e68ff554eb76861a22b2761646
mitugui <cristian.mitugui04@gmail.com>
2025-08-05 13:40:09 -0300

    feat: add Footer component to Default layout page - Remove unused Default.css.

---
+++ src/layouts/Footer/index.tsx
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer-bottom">
+        © 2025 Inovamar - Escritório de Projetos. Todos os direitos reservados
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
