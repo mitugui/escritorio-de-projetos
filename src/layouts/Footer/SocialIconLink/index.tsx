aae5da5ce5439de677356b1cb3e6d5f8c91efd9e
mitugui <cristian.mitugui04@gmail.com>
2025-08-05 15:00:08 -0300

    feat: add SocialIconLink component for social media icon anchors

---
+++ src/layouts/Footer/SocialIconLink/index.tsx
@@ -0,0 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import type { IconProp } from '@fortawesome/fontawesome-svg-core';
+import './SocialIconLink.css';
+
+type SocialIconLinkProps = {
+  href: string;
+  icon: IconProp;
+};
+
+const SocialIconLink = ({ href, icon }: SocialIconLinkProps) => {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="social-icon-anchor"
+    >
+      <FontAwesomeIcon icon={icon} />
+    </a>
+  );
+};
+
+export default SocialIconLink;
